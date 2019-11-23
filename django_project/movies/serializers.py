b1a1328e5bd718f1f8212a5b5add4527bad696e8
jr31112 <jr435@naver.com>
2019-11-23 18:25:48 +0900

    Delete gitignore

--- django_project/movies/serializers.py
+++ django_project/movies/serializers.py
@@ -4,31 +4,22 @@ from .models import Movie, Genre, People
 class GenreSerializers(serializers.ModelSerializer):
     class Meta:
         model = Genre
-        fields = ['id', 'name']
+        fields = ['name']
 
 class PeopleSerializers(serializers.ModelSerializer):
     class Meta:
         model = People
-        fields = ['id', 'ko_name', 'en_name', 'birthday', 'gender', 'is_alive', 'popularity', 'place_of_birth', 'img_url']
+        fields = ['ko_name', 'en_name', 'birthday', 'gender', 'is_alive', 'popularity', 'place_of_birth', 'img_url']
 
 class MovieSerializers(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'overview', 'release_date', 'img_url', 'country', 'rating', 'popularity']
+        fields = ['title', 'overview', 'release_date', 'img_url', 'country', 'rating', 'popularity']
 
 class MovieUpdateSerializers(serializers.ModelSerializer):
     actors = PeopleSerializers(many=True)
-    genres = GenreSerializers(many=True)
     director = PeopleSerializers()
+    genres = GenreSerializers(many=True)
     class Meta:
         model =  Movie
         fields = MovieSerializers.Meta.fields + ['genres'] + ['director'] + ['actors']
-
-class PersonDetailSerializers(serializers.ModelSerializer):
-    actor_movies = MovieSerializers(many=True)
-    movie_set = MovieSerializers(many=True)
-    class Meta:
-        model = People
-        fields = PeopleSerializers.Meta.fields + ['actor_movies'] +['movie_set']
-
-
---
+++ django_project/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jr435\desktop\준영이와 함께하는 프로젝트\django\django_project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
