f11920ec7f3c6a6034ed784ed2f55052b2488d3e
kimjunyoung <kimjy392@gmail.com>
2019-11-23 15:18:46 +0900

    Django | Add MovieSerializer

--- django_project/movies/urls.py
+++ django_project/movies/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
+    path('movies/', views.movie_index, name='movie_index'),
+    path('genres/', views.genre_index, name='genre_index'),
+    path('people/', views.people_index, name='people_index'),
 ]
