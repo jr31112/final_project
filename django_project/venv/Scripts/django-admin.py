195eb7a2dde3019dbcb2c0bd5ac02c2892ed3620
jr31112 <jr435@naver.com>
2019-11-24 15:24:18 +0900

    remove venv, .vscode

--- django_project/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\jr435\desktop\준영이와 함께하는 프로젝트\django\django_project\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
