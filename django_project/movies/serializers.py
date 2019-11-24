23ea3cfe7b561de94572faacf9527538bd30bd4d
kimjunyoung <kimjy392@gmail.com>
2019-11-24 15:31:37 +0900

    Django Update

--- django_project/movies/serializers.py
+++ django_project/movies/serializers.py
@@ -4,22 +4,31 @@ from .models import Movie, Genre, People
 class GenreSerializers(serializers.ModelSerializer):
     class Meta:
         model = Genre
-        fields = ['name']
+        fields = ['id', 'name']
 
 class PeopleSerializers(serializers.ModelSerializer):
     class Meta:
         model = People
-        fields = ['ko_name', 'en_name', 'birthday', 'gender', 'is_alive', 'popularity', 'place_of_birth', 'img_url']
+        fields = ['id', 'ko_name', 'en_name', 'birthday', 'gender', 'is_alive', 'popularity', 'place_of_birth', 'img_url']
 
 class MovieSerializers(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['title', 'overview', 'release_date', 'img_url', 'country', 'rating', 'popularity']
+        fields = ['id', 'title', 'overview', 'release_date', 'img_url', 'country', 'rating', 'popularity']
 
 class MovieUpdateSerializers(serializers.ModelSerializer):
     actors = PeopleSerializers(many=True)
-    director = PeopleSerializers()
     genres = GenreSerializers(many=True)
+    director = PeopleSerializers()
     class Meta:
         model =  Movie
         fields = MovieSerializers.Meta.fields + ['genres'] + ['director'] + ['actors']
+
+class PersonDetailSerializers(serializers.ModelSerializer):
+    actor_movies = MovieSerializers(many=True)
+    movie_set = MovieSerializers(many=True)
+    class Meta:
+        model = People
+        fields = PeopleSerializers.Meta.fields + ['actor_movies'] +['movie_set']
+
+
