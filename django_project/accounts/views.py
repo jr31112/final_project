793f96a5e04d56dccda914a6195076e406816a61
kimjy392 <kimjy392@gmail.com>
2019-11-25 16:56:36 +0900

    Add signup validator

--- django_project/accounts/views.py
+++ django_project/accounts/views.py
@@ -3,6 +3,8 @@ from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework_jwt.serializers import JSONWebTokenSerializer
+
 # Create your views here.
 @api_view(['GET'])
 def user_detail(request, user_id):
@@ -12,6 +14,11 @@ def user_detail(request, user_id):
 
 @api_view(['POST'])
 def signup(request):
-    user = UserSerializer.create(get_user_model(), request.data)
-    serializer = UserSerializer(user)
-    return Response(serializer.data)
+    error = UserSerializer.validate(get_user_model(), data=request.data)
+    if error['password'] or error['username']:
+        return Response(error, status=400)
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        user = UserSerializer.create(get_user_model(), request.data)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
