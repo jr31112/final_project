aa067f4999c1e451264adb7cba890b63310532e0
kimjy392 <kimjy392@gmail.com>
2019-11-28 14:20:51 +0900

    git error 때문에 수정

--- lastproject/settings/productions.py
+++ lastproject/settings/productions.py
@@ -1,5 +1,7 @@
 from .base import *
 
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = []
+
+import django_heroku
+django_heroku.settings(locals())
--- movies/models.py
+++ movies/models.py
@@ -4,16 +4,16 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 class Genre(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
 
 
 class People(models.Model):
     id = models.IntegerField(primary_key=True)
-    ko_name = models.CharField(max_length=30)
-    en_name = models.CharField(max_length=30)
-    birthday = models.CharField(max_length=30, null=True)
-    deathday = models.CharField(max_length=30, null=True)
-    gender = models.CharField(max_length=10)
+    ko_name = models.CharField(max_length=300)
+    en_name = models.CharField(max_length=300)
+    birthday = models.CharField(max_length=300, null=True)
+    deathday = models.CharField(max_length=300, null=True)
+    gender = models.CharField(max_length=100)
     popularity = models.FloatField()
     place_of_birth = models.TextField(null=True)
     img_url = models.TextField(null=True)
@@ -25,7 +25,7 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     genres = models.ManyToManyField(Genre, related_name='movies')
     overview = models.TextField(null=True)
-    release_date = models.CharField(max_length=30)
+    release_date = models.CharField(max_length=300)
     actors = models.ManyToManyField(People, related_name='actor_movies')
     director = models.ForeignKey(People, on_delete=models.PROTECT)
     img_url = models.TextField(null=True)
