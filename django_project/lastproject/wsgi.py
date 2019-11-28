6b79b49ec29e6cf16cbd3b4111cdeb5b87b2b6e8
jr31112 <jr435@naver.com>
2019-11-28 09:47:23 +0900

    serrings.py 분리

--- django_project/lastproject/wsgi.py
+++ django_project/lastproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lastproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lastproject.settings.productions')
 
 application = get_wsgi_application()
